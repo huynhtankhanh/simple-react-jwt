f046be4a813782a6d63c1ec4631fcddf3f4a2e3b
Khanh Huynh <khanh.huynh@axonactive.com>
2020-04-03 02:02:59 +0700

    First commit

---
+++ routes/auth.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { registerValidation, loginValidation } = require('../validation');
+
+router.post('/register', async (req, res) => {
+    // validate request data
+    const { error } = registerValidation(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // checking if user is already in database
+    const existedEmail = await User.findOne({ email: req.body.email});
+    if (existedEmail) {
+        return res.status(400).send('Email already existed.');
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+    try {
+        const savedUser = await user.save();
+        res.send({user: user._id});
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    // validate request data
+    const { error } = loginValidation(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // checking if user is already in database
+    const user = await User.findOne({ email: req.body.email});
+    if (!user) {
+        return res.status(400).send('Email is not found.');
+    }
+
+    //password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) {
+        return res.status(400).send('Invalid password.');
+    }
+
+    //create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('Authorization', token).send(token);
+});
+
+module.exports = router;
